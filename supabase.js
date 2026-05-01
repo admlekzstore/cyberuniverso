e7d28002b02284f78fcdd00bbb375a74ab80fde5
admlekzstore <admlekzstore@gmail.com>
2026-04-30 22:10:19 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const SUPABASE_URL = 'https://fdqjnqlbjjpeyzzckjux.supabase.co/rest/v1/'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkcWpucWxiampwZXl6emNranV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjEyMTksImV4cCI6MjA5MzEzNzIxOX0.j_uTIfXpAh7xlYESEueHxIvjeXzr9fx96lT-RMd7c2U'
