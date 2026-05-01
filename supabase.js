86cbf0b564894330c425e4f9b69aeceaf91aa658
admlekzstore <admlekzstore@gmail.com>
2026-05-01 10:29:38 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://fdqjnqlbjjpeyzzckjux.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://fdqjnqlbjjpeyzzckjux.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkcWpucWxiampwZXl6emNranV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjEyMTksImV4cCI6MjA5MzEzNzIxOX0.j_uTIfXpAh7xlYESEueHxIvjeXzr9fx96lT-RMd7c2U'
 
-window._supabase = supabase.createClient(https://fdqjnqlbjjpeyzzckjux.supabase.co/rest/v1/, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkcWpucWxiampwZXl6emNranV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjEyMTksImV4cCI6MjA5MzEzNzIxOX0.j_uTIfXpAh7xlYESEueHxIvjeXzr9fx96lT-RMd7c2U)
+window._supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
