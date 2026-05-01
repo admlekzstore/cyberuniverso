d9614f55be0bbb4cf84ddccd92c8351882011c38
admlekzstore <admlekzstore@gmail.com>
2026-04-30 22:15:54 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
 const SUPABASE_URL = 'https://fdqjnqlbjjpeyzzckjux.supabase.co/rest/v1/'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkcWpucWxiampwZXl6emNranV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjEyMTksImV4cCI6MjA5MzEzNzIxOX0.j_uTIfXpAh7xlYESEueHxIvjeXzr9fx96lT-RMd7c2U'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const { createClient } = supabase
+window._supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
