c6dfe838d15d82d8b105b5111abe8a9cdfbbdd93
admlekzstore <admlekzstore@gmail.com>
2026-05-12 18:15:35 -0300

    Add files via upload

--- header.js
+++ header.js
@@ -8,9 +8,9 @@ _gaScript.async = true
 _gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-4TCG0DDRCZ'
 document.head.appendChild(_gaScript)
 window.dataLayer = window.dataLayer || []
-function gtag(){dataLayer.push(arguments)}
-gtag('js', new Date())
-gtag('config', 'G-4TCG0DDRCZ')
+window.gtag = function(){window.dataLayer.push(arguments)}
+window.gtag('js', new Date())
+window.gtag('config', 'G-4TCG0DDRCZ')
 
 (function() {
   var style = document.createElement('style')
